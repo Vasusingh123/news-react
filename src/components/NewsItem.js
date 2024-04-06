a18b35e5576d8bf96c6dc9dfbc347dbd621bc451
Vasusingh123 <146229653+Vasusingh123@users.noreply.github.com>
2024-04-06 07:33:23 +0530

    news layout

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export default class NewsItem extends Component {
     return (
       <div className='my-3'>
        <div className="card" style={{width:"18rem"}}>
-           <img src={imgurl} className="card-img-top" alt="..."/>
+           <img src={!imgurl?"https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_675,q_auto:good,w_1200/cms/uploads/wq0rqdi36zotj6akl00b":imgurl} className="card-img-top" alt="..."/>
       <div className="card-body">
            <h5 className="card-title">{title}...</h5>
            <p className="card-text">{description}...</p>
-           <a href={newsurl} target="_blank" className="btn btn-sm btn-primary"> Browse </a>
+           <a  rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-dark"> Browse </a>
      </div>
      </div>
      </div>
