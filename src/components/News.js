d500141238207b656371520d730313f469893f44
Vasusingh123 <146229653+Vasusingh123@users.noreply.github.com>
2024-04-08 08:35:56 +0530

    pageset

--- src/components/News.js
+++ src/components/News.js
@@ -9,33 +9,47 @@ export default class News extends Component {
     this.state={
       articles : [],
       loading : false,
-      page :1
+      page :0
     }
   }
-  async componentDidMount (){
-     let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=135e3a4a5846493ea19137d7157df39d";
-     let data= await fetch(url);
-     let parseddata=await data.json();
-     console.log(parseddata); 
-     this.setState({
+  componentDidMount = async()=>{
+    let url=`https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=135e3a4a5846493ea19137d7157df39d&pagesize=20`;
+    let data= await fetch(url);
+    let parseddata=await data.json();
+    this.setState({
+     
       articles:parseddata.articles
      })
   }
-  prevfun = ()=>{
-   console.log("prev");
+  
+  prevfun = async()=>{
+    let url=`https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=135e3a4a5846493ea19137d7157df39d&page=${this.state.page-1}&pagesize=20`;
+    let data= await fetch(url);
+    let parseddata=await data.json();
+   this.setState({
+    page:this.state.page-1,
+    articles:parseddata.articles
+   })
   }
-  nextfun = ()=>{
-    console.log("next");
+  nextfun = async()=>{
+    let url=`https://newsapi.org/v2/everything?q=tesla&from=2024-03-08&sortBy=publishedAt&apiKey=135e3a4a5846493ea19137d7157df39d&page=${this.state.page+1}&pagesize=20`;
+    let data= await fetch(url);
+    let parseddata=await data.json();
+    this.setState({
+      page:this.state.page+1,
+      articles:parseddata.articles
+     })
    }
+   
   render() {
-
+   
     return (
       <div className='container my-3'>
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
           {this.state.articles.map((element)=>{
             return  <div className="col-md-4" key={element.url}>
-            <NewsItem  description={element.description?element.description.slice(0,88):""} title={element.title?element.title.slice(0,44):""} imgurl={element.urlToImage} newsurl={element.url} />        
+            <NewsItem  description={(element.description!=='[Removed]')&& element.description?element.description.slice(0,88):"This page id being empty"} title={(element.title!=='[Removed]')&& element.title?element.title.slice(0,44):"This page is empty"} imgurl={element.urlToImage ? element.urlToImage:"https://static.toiimg.com/thumb/msid-109085602,imgsize-118244,width-400,resizemode-4/109085602.jpg"} newsurl={element.url} />        
             </div> 
           })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
     return (
       <div className='my-3'>
        <div className="card" style={{width:"18rem"}}>
-           <img src={!imgurl?"https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_675,q_auto:good,w_1200/cms/uploads/wq0rqdi36zotj6akl00b":imgurl} className="card-img-top" alt="..."/>
+           <img src={!imgurl?"https://newsapi.org/v2/everything?domains=wsj.com&apiKey=135e3a4a5846493ea19137d7157df39d":imgurl} className="card-img-top" alt="..."/>
       <div className="card-body">
            <h5 className="card-title">{title}...</h5>
            <p className="card-text">{description}...</p>
